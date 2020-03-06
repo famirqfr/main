4c574dabc28ca0a3e9101ce1aab3dfbbd2482cc3
Mahdi <mahditorabi28@gmail.com>
2020-03-06 21:10:07 +0330

    Added Float Button to Report Fragment

--- app/src/main/java/ir/qwerty/myapplication/SplashActivity.java
+++ app/src/main/java/ir/qwerty/myapplication/SplashActivity.java
@@ -1,10 +1,20 @@
 package ir.qwerty.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,10 +26,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },  1000);
+        }, 1000);
     }
 }
