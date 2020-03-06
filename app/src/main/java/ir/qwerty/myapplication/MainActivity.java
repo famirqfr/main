4c9c867b9862d8befa0dcb078515fb3911a9a608
Famirqfr <amqfr1228@gmail.com>
2020-03-06 17:58:41 +0330

    first commit

--- app/src/main/java/ir/qwerty/myapplication/MainActivity.java
+++ app/src/main/java/ir/qwerty/myapplication/MainActivity.java
@@ -5,22 +5,32 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.squareup.picasso.Picasso;
 
 import ir.qwerty.myapplication.fragment.LocationFragment;
 import ir.qwerty.myapplication.fragment.MessageFragment;
+import ir.qwerty.myapplication.fragment.NewsFragment;
 import ir.qwerty.myapplication.fragment.ReportFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,31 +41,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar=findViewById(R.id.aa);
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("hello");
-        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName("bye");
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("R.string.drawer_item_home");
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName("R.string.drawer_item_settings");
 
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withSelectionListEnabledForSingleProfile(false)
+                .withHeaderBackground(R.drawable.drawer_background)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(getResources().getDrawable(R.drawable.ic_launcher_background))
+                )
+                .build();
+        //create the drawer and remember the `Drawer` result object
         Drawer result = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
                 .addDrawerItems(
                         item1,
                         new DividerDrawerItem(),
                         item2,
-                        new SecondaryDrawerItem().withName("sey good")
+                        new SecondaryDrawerItem().withName("R.string.drawer_item_settings")
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
                         return false;
                     }
                 })
+                .withSelectedItem(-1)
                 .build();
+
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                Picasso.get().load(uri).placeholder(placeholder).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                Picasso.get().cancelRequest(imageView);
+            }
+        });
 
         FragmentPagerAdapter adapter = new FragmentPagerItemAdapter(
                 getSupportFragmentManager(), FragmentPagerItems.with(this)
                 .add("پیام", MessageFragment.class)
                 .add("گزارش", ReportFragment.class)
-                .add("اخبار", MessageFragment.class)
+                .add("اخبار", NewsFragment.class)
                 .add("موقعیت", LocationFragment.class)
                 .create());
 
